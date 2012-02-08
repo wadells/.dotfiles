bc1a8bf7d13632a4ca6240a7096dbe418e343a50
Walt Javins <walt@javins.net>
2012-02-07 23:08:09 -0800

    History cleanup.

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,13 +1,12 @@
 ## Command history configuration
-HISTFILE=~/.histfile
-HISTSIZE=10000
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
 SAVEHIST=10000
 
-setopt append_history
 setopt extended_history
-setopt hist_expire_dups_first
+setopt append_history
+setopt inc_append_history
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
-setopt hist_verify
-setopt inc_append_history
-setopt share_history # share command history data
+setopt hist_verify # when using !history commands, ask for a return before executing
+setopt hist_no_store # ignore history commands
