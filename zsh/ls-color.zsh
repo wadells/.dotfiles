1753366259989376817561e78bfa7fd6ee6e4f45
Walt Javins <walt@javins.net>
2012-02-13 16:10:36 -0800

    Solarized ls colors for linux and bsd.

--- zsh/ls-color.zsh
+++ zsh/ls-color.zsh
@@ -1,9 +1,8 @@
 if ls --color -d . > /dev/null 2>&1; then
     alias ls='ls --color=auto'
+    export LS_COLORS="di=34:ln=36:so=35;40:pi=1;35;40:ex=33:bd=32;40:cd=33;40:su=31:sg=1;31:tw=1;35:ow=35:or=31;40:"
 elif ls -G -d . > /dev/null 2>&1; then
     alias ls='ls -G'
-fi
-
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval
+    export CLICOLOR=1
+    export LSCOLORS=exgxfaFadxcadabxBxFxfx
 fi
