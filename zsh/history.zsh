db97c39e3b6913f3968c00b157d450b0642bf82d
Walt Javins <walt@javins.net>
2012-02-28 16:09:31 -0800

    Added hist() function that greps history.

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -10,3 +10,12 @@ setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
 setopt hist_verify # when using !history commands, ask for a return before executing
 setopt hist_no_store # ignore history commands
+
+funtion hist () {
+if [ -z $* ]
+	then
+		history 1
+	else
+		history 1 | grep -E $@
+	fi
+}
