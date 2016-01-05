396e6b5d77ed76062941dd6862e6b151993eb823
Walt Javins <walt@javins.net>
2016-01-05 04:13:20 +0000

    Organize nixpkgs into different sections.
    Some attempt at seperating out what is useful for headless vs gui
    configurations.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,32 +1,49 @@
 {
-  allowUnfree = true;  # for chromium plugins
 
+  allowUnfree = true;  # for chromium plugins
   packageOverrides = pkgs_: with pkgs_; {
-    walts = with pkgs; buildEnv {
-      name = "walts";
+
+    linux-desktop-pkgs = with pkgs; buildEnv {
+      name = "linux-desktop-pkgs";
       paths = [
-        # system utils
+        (chromium.override {
+          enablePepperFlash = true;
+        })
+        inconsolata
+      ];
+    };
+
+    linux-cli-pkgs = with pkgs; buildEnv {
+      name = "linux-cli-pkgs";
+      paths = [
+        # "standard" system utils
+        lsof
         xorg.xmodmap  # rebind capslock -> esc
 
-        # cli
+        # hw introspection/monitoring
+        lm_sensors
+
+        # my extra system utils
+        htop
+        psmisc
+        tree
+
+        # cli / dev tools
         ack
         git
         gnupg
         irssi
         mosh
         tmux
-        tree
-
-        # destop
-        (chromium.override {
-          enablePepperFlash = true;
-        })
-        inconsolata
-
-        # languages
-        python
-
       ];
     };
+
+    linux-languages = with pkgs; buildEnv {
+      name = "linux-languages";
+      paths = [
+        python
+      ];
+    };
+
   };
 }
